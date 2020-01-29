667c10a238ef6e863814d06591aa26d1bd10b9f1
khkim <wbkifun@nate.com>
2020-01-30 00:02:13 +0900

    04.1110 and runcpp

--- 04.while/1110_cycle_add.cpp
+++ 04.while/1110_cycle_add.cpp
@@ -4,25 +4,32 @@
 using namespace std;
 
 int main() {
+    int a, b, c, cnt;
+    string s;
+    scanf("%d", &a);
+
+    b = a;
+    cnt = 0;
+
+    while (true) {
+        cnt++;
+
+        if (b < 10) {
+            c = b*11;
+        }
+        else {
+            s = to_string(b);
+            //cout << "s " << s << endl;
+            //cout << int(s[0]) << ", " << int(s[1]) << endl;
+            c = (b%10)*10 + (int(s[0])-48 + int(s[1])-48)%10;
+        }
+
+        //cout << "#" << cnt << " " << c << endl;
+
+        if (c == a) break;
+        else b = c;
+    }
+    cout << cnt << endl;
 
     return 0;
 }
-
-n = input()
-a = n[:]
-cnt = 1
-
-while True:
-    if len(a) == 1:
-        a = '0'+a
-
-    b = a[-1] + str(int(a[0]) + int(a[1]))[-1]
-    print(b)
-
-    if (int(b) == int(n)):
-        break
-    else:
-        a = b[:]
-        cnt += 1
-
-print(cnt)
--- 04.while/1110_cycle_add.py
+++ 04.while/1110_cycle_add.py
@@ -2,19 +2,20 @@
 
 n = input()
 a = n[:]
-cnt = 1
+cnt = 0
 
 while True:
+    cnt += 1;
+
     if len(a) == 1:
         a = '0'+a
 
     b = a[-1] + str(int(a[0]) + int(a[1]))[-1]
-    print(b)
+    print("#{} {}".format(cnt, b))
 
     if (int(b) == int(n)):
         break
     else:
         a = b[:]
-        cnt += 1
 
 print(cnt)
